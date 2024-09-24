f47d4985e90edf3c96242d9b4b888953c49f79f1
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-09-24 12:03:51 +0700

    Login

--- Ecommerce.UI/Program.cs
+++ Ecommerce.UI/Program.cs
@@ -1,41 +1,26 @@
 ﻿using Ecommerce.UI.Components;
 using Ecommerce.UI.Services;
-using Microsoft.AspNetCore.Components;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-// Đăng ký Razor Components với hỗ trợ tương tác
+//builder.Services.AddScoped<HttpClient>(x =>
+//{
+//    var Uri = x.GetRequiredService<NavigationManager>();
+//    HttpClient Client = new()
+//    {
+//        BaseAddress = new Uri("https://localhost:7105/"),
+//    };
+//    Client.DefaultRequestHeaders.Add("Accept", "application/json");
+//    return Client;
+//});
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7105/") });
+
+
 builder.Services.AddRazorComponents()
         .AddInteractiveServerComponents();
-
-// Đọc URL API từ appsettings.Development.json
-//var apiBaseUrl = builder.Configuration.GetValue<string>("ApiSettings:ApiUrl");
-
-//builder.Services.AddHttpClient("ServerAPI", client =>
-//{
-//    client.BaseAddress = new Uri(apiBaseUrl);
-//});
-//builder.Services.AddHttpClient("WebApiClient", client =>
-//{
-//    client.BaseAddress = new Uri("https://localhost:7105/"); // Đặt URL trỏ đến Web API
-//});
-
-builder.Services.AddScoped<HttpClient>(x =>
-{
-    var Uri = x.GetRequiredService<NavigationManager>();
-    HttpClient Client = new()
-    {
-        BaseAddress = new Uri("https://localhost:7105/"),
-    };
-    Client.DefaultRequestHeaders.Add("Accept", "application/json");
-    return Client;
-});
-
-
 // Đăng ký AccountService
+
 builder.Services.AddScoped<AccountService>();
 
 var app = builder.Build();
