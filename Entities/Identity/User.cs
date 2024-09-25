3712282cdc54ec46356c51bac087984bd4fb69ae
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-09-25 18:37:59 +0700

    verificationCode Register, Emailconfirm

--- Entities/Identity/User.cs
+++ Entities/Identity/User.cs
@@ -6,5 +6,6 @@ namespace Entities.Identity
     {
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        public string? VerificationCode { get; set; }
     }
 }
