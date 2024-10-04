dc1ac92d76a96a2e7349c509771af43caf43759a
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-10-04 14:50:27 +0700

    product Details

--- Ecommerce.UI/Services/ProductServices.cs
+++ Ecommerce.UI/Services/ProductServices.cs
@@ -1,5 +1,10 @@
-﻿using Shared.DTO.Product;
+﻿using Shared.DTO.Banner;
+using Shared;
+using Shared.DTO.Product;
 using Shared.DTO.Response;
+using Shared.DTO.BannerProduct;
+using System.Text.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ecommerce.UI.Services
 {
@@ -11,6 +16,34 @@ namespace Ecommerce.UI.Services
         {
             _httpClient = httpClient;
         }
+        public async Task<IEnumerable<BannerProductDto>> GetAllBannerPositionProductPopup(BannerProductWithPopupPosition position)
+        {
+            var response = await _httpClient.GetAsync($"/api/BannerProduct/GetAllBannerPositionProductPopup?position={position}");
+            response.EnsureSuccessStatusCode();
 
+            var apiResponse = await response.Content.ReadFromJsonAsync<ApiResponse<IEnumerable<BannerProductDto>>>();
+            if (apiResponse != null && apiResponse.Success)
+            {
+                return apiResponse.Data;
+            }
+            return new List<BannerProductDto>();
+        }
+        public async Task<ApiResponse<ProductResponseDto>> GetAllProductsPagination(int pageNumber, int pageSize)
+        {
+            var response = await _httpClient.GetAsync($"/api/Product/GetAllProductsPagination?pageNumber={pageNumber}&pageSize={pageSize}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadFromJsonAsync<ApiResponse<ProductResponseDto>>();
+                return data!;
+            }
+
+            return new ApiResponse<ProductResponseDto>();
+        }
+        public async Task<ApiResponse<ProductDto>> GetProductById(Guid id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<ApiResponse<ProductDto>>($"/api/Product/GetProductById/{id}");
+            return response;
+        }
     }
 }
--- Shared/DTO/Response/ApiResponse.cs
+++ Shared/DTO/Response/ApiResponse.cs
@@ -1,9 +1,16 @@
-﻿namespace Shared.DTO.Response
+﻿using Shared.DTO.Product;
+
+namespace Shared.DTO.Response
 {
     public class ApiResponse<T>
     {
         public bool Success { get; set; }
         public string Message { get; set; }
         public T Data { get; set; }
+    }
+    public class ProductResponseDto
+    {
+        public int TotalCount { get; set; }
+        public IEnumerable<ProductDto> Products { get; set; }
     }
 }
