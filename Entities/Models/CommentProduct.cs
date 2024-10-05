0cb9c785733e2e91a03b08a3e9bee8c79f54264c
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-10-05 14:37:07 +0700

    comment Product

--- Entities/Models/CommentProduct.cs
+++ Entities/Models/CommentProduct.cs
@@ -8,10 +8,12 @@ namespace Entities.Models
         public Guid Id { get; set; }
         [Required]
         public string UserId { get; set; }
-        public User User { get; set; }
+        public User User { get; set; } 
         [Required]
         public Guid ProductId { get; set; }
+        public string UserName { get; set; }
         public Product Product { get; set; }
+        public string ProductName { get; set; }
         [Required]
         [StringLength(1000)]
         public string Content { get; set; }
