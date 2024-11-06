6850b3bfcc9a34eb9c32c588ec6d3b4da22135c6
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-11-07 03:54:06 +0700

    crud CategoriesProduct

--- Admin_Wolmart.UI/Services/CateProductServices.cs
+++ Admin_Wolmart.UI/Services/CateProductServices.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.WebUtilities;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.WebUtilities;
 using Shared.DTO.CategoryIntroduce;
 using Shared.DTO.CateProduct;
+using Shared.DTO.Introduce;
 using Shared.DTO.Response;
+using System.Net.Http.Headers;
 
 namespace Admin_Wolmart.UI.Services
 {
@@ -57,6 +60,51 @@ namespace Admin_Wolmart.UI.Services
             var query = $"/api/CateProduct/UpdateCategoryProductStatus/{id}?Status={status}";
             var response = await _httpClient.PutAsJsonAsync(query, updateStatus);
 
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteCategoryProductAsync(Guid id)
+        {
+            var response = await _httpClient.DeleteAsync($"/api/CateProduct/DeleteCategoryProduct/{id}");
+
+            return response.IsSuccessStatusCode;
+
+        }
+
+        public async Task<bool> UpdateCategoryIntroduceAsync(Guid id, UpdateCateProductDto request)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"/api/CateProduct/UpdateCategoryProductStatus/{id}", request);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<ApiResponse<CateProductDto>> GetCateProductByIdAsync(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"/api/CateProduct/GetCategoryProductByCategoryId/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var category = await response.Content.ReadFromJsonAsync<ApiResponse<CateProductDto>>();
+                return category;
+            }
+            return null;
+        }
+
+        public async Task<bool> UpdateCateProductAsync(Guid id, UpdateCateProductDto request, IBrowserFile? file)
+        {
+            var content = new MultipartFormDataContent();
+
+            // Thêm các trường khác vào form-data
+            content.Add(new StringContent(request.Name ?? ""), "Name");
+
+            // Đọc và thêm file vào form-data
+            if (file != null)
+            {
+                var fileContent = new StreamContent(file.OpenReadStream(10485760)); // 10MB limit
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+                content.Add(fileContent, "File", file.Name);
+            }
+
+            var response = await _httpClient.PutAsync($"/api/CateProduct/UpdateCategoryProductStatus/{id}", content);
             return response.IsSuccessStatusCode;
         }
     }
