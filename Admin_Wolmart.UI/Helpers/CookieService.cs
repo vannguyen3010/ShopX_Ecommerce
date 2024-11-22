6551102c69d6ae75fc3f7676a4832ca883d1ef53
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-11-22 23:51:11 +0700

    Authentication (2)

---
+++ Admin_Wolmart.UI/Helpers/CookieService.cs
@@ -0,0 +1,28 @@
+﻿namespace Admin_Wolmart.UI.Helpers
+{
+    public class CookieService(IHttpContextAccessor httpContextAccessor)
+    {
+
+        public void SetCookie(string key, string value, int expiryInMinutes)
+        {
+            var cookieOptions = new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = true,
+                Expires = DateTimeOffset.Now.AddMinutes(expiryInMinutes)
+            };
+
+            httpContextAccessor.HttpContext?.Response.Cookies.Append(key, value, cookieOptions);
+        }
+
+        public string? GetCookie(string key)
+        {
+            return httpContextAccessor.HttpContext?.Request.Cookies[key];
+        }
+
+        public void DeleteCookie(string key)
+        {
+            httpContextAccessor.HttpContext?.Response.Cookies.Delete(key);
+        }
+    }
+}
