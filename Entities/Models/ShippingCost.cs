75d8e215c7c0482dd801d8f7a011042104f4abc5
NguyênDev <114875577+vannguyen3010@users.noreply.github.com>
2024-12-16 13:27:25 +0700

    UpdateStatusShippingCost

--- Entities/Models/ShippingCost.cs
+++ Entities/Models/ShippingCost.cs
@@ -9,6 +9,7 @@ namespace Entities.Models
         public string ProvinceCode { get; set; }
         public string ProvinceName { get; set; }
         public decimal Cost { get; set; }
+        public bool Status { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
